6be405cee4cc475b1e32354085961233583d2458
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-28 21:59:40 +0200

    init categories DBs

--- without_restApi/database/migrations/2018_02_28_074100_create_subcategories_table.php
+++ without_restApi/database/migrations/2018_02_28_074100_create_subcategories_table.php
@@ -15,6 +15,8 @@ class CreateSubcategoriesTable extends Migration
     {
         Schema::create('subcategories', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('category_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
