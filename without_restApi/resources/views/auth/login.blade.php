d8a1e56c9a331ab89f15d77fc9a19393da0bd4e2
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-28 09:30:46 +0200

    login using facebook

--- without_restApi/resources/views/auth/login.blade.php
+++ without_restApi/resources/views/auth/login.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
 	<div class="col-md-12 center">
-        <form action="{{route('postLogin')}}" method="post">
-	        <input class="form-control" placeholder="Email" name="email">
-	        <input class="form-control" placeholder="Password" name="password" type="password">
-	        <button type="submit" class="btn btn-default">login</button>
-	        <input type="hidden" name="_token" value="{{ Session::token() }}">
-		</form>		
+		<a href="{{route('login-provider','facebook')}}">login using facebook</a>	
 	</div>
 @endsection
